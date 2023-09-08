6481bcd809e5dd4cb5b6b2b43e163855b7447868
Joseph Ojo <70476696+OjoFemiJoseph@users.noreply.github.com>
2023-09-08 19:49:35 +0100

    Update __init__.py

--- python/get-key-by-name/__init__.py
+++ python/get-key-by-name/__init__.py
@@ -6,9 +6,20 @@ from azure.identity import DefaultAzureCredential
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    print('uneccessary new additin')
     logging.info('Python HTTP trigger function processed a request.')
-    
+    have_vault_name = False
+    have_secret_name = False
+    error_retrieving_secret = False
+    try:
+        req_body = req.get_json()
+        print(req_body)
+    except ValueError:
+         pass
+    else:
+        #Get Vault name from body
+        vault_name = req_body["data"][0][1]
+        logging.info(f'Got value {vault_name} for vault_name from request body.')
+        have_vault_name = True
     try:
         req_body = req.get_json()
     except ValueError:
